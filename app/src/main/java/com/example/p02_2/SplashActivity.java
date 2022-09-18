010f32eafb2b441d0efc59b8201a901c660ce9cb
YourName <email@example.com>
2022-09-18 22:14:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/p02_2/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.p02_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+    private  Runnable runnableToMainActivity=new Runnable() {
+        @Override
+        public void run() {
+            toMainActivity();
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        handler.postDelayed(runnableToMainActivity,3000);
+    }
+//    跳转到主界面
+    private void toMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        //防止内存泄漏
+        handler.removeCallbacks(runnableToMainActivity);
+    }
+}
